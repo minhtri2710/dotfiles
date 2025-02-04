b2daa485d1224e100096e2a9f28d68c06c9fc095
Tri.Tran <inpro2710@gmail.com>
2025-02-04 23:02:37 +0700

    docs: update config nvim and tmux

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
-  {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {},
--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -29,31 +29,9 @@ return {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
       },
-      "nvim-telescope/telescope-file-browser.nvim",
       "ThePrimeagen/git-worktree.nvim",
-      "debugloop/telescope-undo.nvim",
-      "nvim-telescope/telescope-frecency.nvim",
     },
     keys = {
-      {
-        "sf",
-        function()
-          local function telescope_buffer_dir()
-            return vim.fn.expand("%:p:h")
-          end
-
-          require("telescope").extensions.file_browser.file_browser({
-            path = "%:p:h",
-            cwd = telescope_buffer_dir(),
-            respect_gitignore = false,
-            hidden = true,
-            grouped = true,
-            initial_mode = "normal",
-            layout_config = { height = 40 },
-          })
-        end,
-        desc = "Open File Browser with the path of the current buffer",
-      },
       {
         "<leader>gw",
         function()
@@ -61,33 +39,10 @@ return {
         end,
         desc = "List git worktree",
       },
-      {
-        ";p",
-        function()
-          require("telescope").extensions.pomodori.timers()
-        end,
-        desc = "Manage pomodori timers",
-      },
-      {
-        ";u",
-        function()
-          require("telescope").extensions.undo.undo()
-        end,
-        desc = "Manage undo",
-      },
-      {
-        "<leader>sf",
-        function()
-          require("telescope").extensions.frecency.frecency()
-        end,
-        desc = "Frecency",
-      },
     },
     config = function(_, opts)
       local telescope = require("telescope")
       local actions = require("telescope.actions")
-      local fb_actions = require("telescope").extensions.file_browser.actions
-      local fb_config = require("telescope").extensions.file_browser.config
 
       opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
         wrap_results = true,
@@ -112,49 +67,9 @@ return {
           },
         },
       }
-      opts.extensions = {
-        file_browser = {
-          -- disables netrw and use telescope-file-browser in its place
-          hijack_netrw = true,
-          mappings = {
-            -- your custom insert mode mappings
-            ["n"] = {
-              -- your custom normal mode mappings
-              ["N"] = fb_actions.create,
-              ["h"] = fb_actions.goto_parent_dir,
-              ["/"] = function()
-                vim.cmd("startinsert")
-              end,
-              ["<C-u>"] = function(prompt_bufnr)
-                for i = 1, 10 do
-                  actions.move_selection_previous(prompt_bufnr)
-                end
-              end,
-              ["<C-d>"] = function(prompt_bufnr)
-                for i = 1, 10 do
-                  actions.move_selection_next(prompt_bufnr)
-                end
-              end,
-              ["<PageUp>"] = actions.preview_scrolling_up,
-              ["<PageDown>"] = actions.preview_scrolling_down,
-            },
-          },
-        },
-        undo = {
-          side_by_side = true,
-          layout_strategy = "vertical",
-          layout_config = {
-            preview_height = 0.8,
-          },
-        },
-      }
       telescope.setup(opts)
       telescope.load_extension("fzf")
-      telescope.load_extension("file_browser")
       telescope.load_extension("git_worktree")
-      telescope.load_extension("pomodori")
-      telescope.load_extension("undo")
-      telescope.load_extension("frecency")
     end,
   },
   {
