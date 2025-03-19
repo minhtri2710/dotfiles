c2d1982c816b80edf22af7485248db00cc4a823b
Tri.Tran <inpro2710@gmail.com>
2025-03-19 22:54:26 +0700

    feat: :sparkles: update config snacks.nvim

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,32 +1,35 @@
 return {
-	{
-		"folke/snacks.nvim",
-		priority = 1000,
-		lazy = false,
-		---@type snacks.Config
-		opts = {
-			picker = {
-				layout = {
-					layout = {
-						backdrop = true,
-					},
-				},
-				formatters = {
-					file = {
-						filename_first = true,
-					},
-				},
-			},
-			statuscolumn = { enabled = true },
-			image = {
-				enabled = true,
-				doc = {
-					inline = true,
-				},
-			},
-			dashboard = {
-				preset = {
-					header = [[
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      picker = {
+        layout = {
+          layout = {
+            backdrop = true,
+          },
+        },
+        formatters = {
+          file = {
+            filename_first = true,
+          },
+        },
+      },
+      statuscolumn = { enabled = true },
+      image = {
+        enabled = true,
+        doc = {
+          inline = false,
+          float = true,
+          max_width = 60,
+          max_height = 30,
+        },
+      },
+      dashboard = {
+        preset = {
+          header = [[
 ████████╗██████╗ ██╗    ████████╗██████╗  █████╗ ███╗   ██╗
 ╚══██╔══╝██╔══██╗██║    ╚══██╔══╝██╔══██╗██╔══██╗████╗  ██║
    ██║   ██████╔╝██║       ██║   ██████╔╝███████║██╔██╗ ██║
@@ -34,83 +37,83 @@ return {
    ██║   ██║  ██║██║       ██║   ██║  ██║██║  ██║██║ ╚████║
    ╚═╝   ╚═╝  ╚═╝╚═╝       ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
 ]],
-				},
-			},
-		},
-		keys = {
-			{
-				"<leader>fP",
-				function()
-					Snacks.picker.files({ cwd = require("lazy.core.config").options.root })
-				end,
-				desc = "Find Plugin File",
-			},
-			{
-				";f",
-				function()
-					Snacks.picker.files({ layout = {
-						preview = false,
-					} })
-				end,
-				desc = "Lists files in your current working directory, respects .gitignore",
-			},
-			{
-				";s",
-				function()
-					Snacks.picker.smart({ layout = {
-						preview = false,
-					} })
-				end,
-				desc = "Lists files in your current working directory, respects .gitignore",
-			},
-			{
-				";r",
-				function()
-					Snacks.picker.grep({ hidden = true })
-				end,
-				desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore",
-			},
-			{
-				";;",
-				function()
-					Snacks.picker.resume()
-				end,
-				desc = "Resume the previous telescope picker",
-			},
-			{
-				"sf",
-				function()
-					Snacks.picker.explorer({
-						cwd = vim.fn.expand("%:p:h"),
-						auto_close = true,
-						layout = {
-							preset = "vertical",
-						},
-					})
-				end,
-				desc = "Open File Browser with the path of the current buffer",
-			},
-			{
-				";u",
-				function()
-					Snacks.picker.undo()
-				end,
-				desc = "Undo history",
-			},
-			{
-				";g",
-				function()
-					Snacks.picker.git_files()
-				end,
-				desc = "Git files",
-			},
-			{
-				";gb",
-				function()
-					Snacks.picker.git_branches()
-				end,
-				desc = "Git branches",
-			},
-		},
-	},
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>fP",
+        function()
+          Snacks.picker.files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+      {
+        ";f",
+        function()
+          Snacks.picker.files({ layout = {
+            preview = false,
+          } })
+        end,
+        desc = "Lists files in your current working directory, respects .gitignore",
+      },
+      {
+        ";s",
+        function()
+          Snacks.picker.smart({ layout = {
+            preview = false,
+          } })
+        end,
+        desc = "Lists files in your current working directory, respects .gitignore",
+      },
+      {
+        ";r",
+        function()
+          Snacks.picker.grep({ hidden = true })
+        end,
+        desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore",
+      },
+      {
+        ";;",
+        function()
+          Snacks.picker.resume()
+        end,
+        desc = "Resume the previous telescope picker",
+      },
+      {
+        "sf",
+        function()
+          Snacks.picker.explorer({
+            cwd = vim.fn.expand("%:p:h"),
+            auto_close = true,
+            layout = {
+              preset = "vertical",
+            },
+          })
+        end,
+        desc = "Open File Browser with the path of the current buffer",
+      },
+      {
+        ";u",
+        function()
+          Snacks.picker.undo()
+        end,
+        desc = "Undo history",
+      },
+      {
+        ";g",
+        function()
+          Snacks.picker.git_files()
+        end,
+        desc = "Git files",
+      },
+      {
+        ";gb",
+        function()
+          Snacks.picker.git_branches()
+        end,
+        desc = "Git branches",
+      },
+    },
+  },
 }
