0963f2d2847599fc67bf843f04ecfecdda331668
Tri.Tran <inpro2710@gmail.com>
2025-05-31 14:26:46 +0700

    chore: :hammer: update config

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -1,31 +1,28 @@
 return {
-	{
-		"epwalsh/obsidian.nvim",
-		version = "*",
-		lazy = true,
-		ft = "markdown",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		opts = {
-			workspaces = {
-				{
-					name = "Obsidian",
-					path = function()
-						if (vim.fn.has("win32")) == 1 then
-							return "C:/Users/tri.tran/Downloads/Obsidian/Beowulf"
-						end
+  {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      ui = {
+        enable = false,
+      },
+      workspaces = {
+        {
+          name = "Obsidian",
+          path = function()
+            if (vim.fn.has("win32")) == 1 then
+              return "C:/Users/tri.tran/Downloads/Obsidian/Beowulf"
+            end
 
-						return "~/Documents/Obsidian Vault/"
-					end,
-				},
-			},
-			ui = {
-				enable = false,
-			},
-			daily_notes = {
-				folder = "dailies",
-			},
-		},
-	},
+            return "~/Documents/obsidian/Second Brain/"
+          end,
+        },
+      },
+    },
+  },
 }
