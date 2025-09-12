d143da4eb2eb649da64f81945f1f4899e1f4365b
Tri.Tran <inpro2710@gmail.com>
2025-09-12 10:07:09 +0700

    feat: :sparkles: revert to obsidian.nvim

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,22 +1,22 @@
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  command = "set nopaste",
+	pattern = "*",
+	command = "set nopaste",
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "php",
-  callback = function()
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.tabstop = 4
-    vim.opt_local.wrap = true
-    vim.opt_local.expandtab = false
-  end,
+	pattern = "php",
+	callback = function()
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.tabstop = 4
+		vim.opt_local.wrap = true
+		vim.opt_local.expandtab = false
+	end,
 })
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "phtml",
-  callback = function()
-    vim.b.autoformat = false
-  end,
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*.phtml",
+	callback = function()
+		vim.b.autoformat = false
+	end,
 })
--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,107 +1,92 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    enabled = false,
-  },
-  {
-    "brianhuster/live-preview.nvim",
-    dependencies = {
-      "folke/snacks.nvim",
-    },
-  },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      completions = {
-        lsp = {
-          enabled = true,
-        },
-        blink = {
-          enabled = true,
-        },
-      },
-      block = {
-        sign = true,
-      },
-      heading = {
-        sign = true,
-        position = "inline",
-        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
-      },
-      checkbox = {
-        enabled = true,
-        unchecked = { icon = "✘ " },
-        checked = { icon = "✔ ", scope_highlight = "@markup.strikethrough" },
-        custom = { todo = { rendered = "◯ " } },
-      },
-    },
-  },
-  {
-    "tadmccorkle/markdown.nvim",
-    ft = "markdown",
-    opts = {},
-  },
-  {
-    "obsidian-nvim/obsidian.nvim",
-    version = "*",
-    lazy = true,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    opts = {
-      ui = {
-        enable = false,
-      },
-      open_notes_in = "vsplit",
-      note_path_func = function(spec)
-        local absolute_dir = "/Users/beowulf/Documents/notes"
-        local path = require("obsidian.path")
-        local new_path = path.new(absolute_dir) / tostring(spec.id)
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {
+			completions = {
+				lsp = {
+					enabled = true,
+				},
+				blink = {
+					enabled = true,
+				},
+			},
+			block = {
+				sign = true,
+			},
+			heading = {
+				sign = true,
+				position = "inline",
+				icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+			},
+			checkbox = {
+				enabled = true,
+				unchecked = { icon = "✘ " },
+				checked = { icon = "✔ ", scope_highlight = "@markup.strikethrough" },
+				custom = { todo = { rendered = "◯ " } },
+			},
+		},
+	},
+	{
+		"tadmccorkle/markdown.nvim",
+		ft = "markdown",
+		keys = {},
+		opts = {},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				marksman = {
+					enabled = false,
+				},
+			},
+			setup = {
+				markdown_oxide = function(_, opts)
+					LazyVim.lsp.on_attach(function(client, bufnr)
+						local function check_codelens_support()
+							if client.server_capabilities.codeLensProvider then
+								return true
+							end
+							return false
+						end
 
-        return new_path:with_suffix(".md")
-      end,
-
-      workspaces = {
-        {
-          name = "no-vault",
-          path = function()
-            return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
-          end,
-        },
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        marksman = {
-          enabled = false,
-        },
-      },
-      setup = {
-        markdown_oxide = function(_, opts)
-          LazyVim.lsp.on_attach(function(client, bufnr)
-            local function check_codelens_support()
-              if client.server_capabilities.codeLensProvider then
-                return true
-              end
-              return false
-            end
-
-            vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave", "CursorHold", "LspAttach", "BufEnter" }, {
-              buffer = bufnr,
-              callback = function()
-                if check_codelens_support() then
-                  vim.lsp.codelens.refresh({ bufnr = bufnr })
-                end
-              end,
-            })
-            vim.api.nvim_exec_autocmds("User", { pattern = "LspAttached" })
-          end)
-        end,
-      },
-    },
-  },
+						vim.api.nvim_create_autocmd(
+							{ "TextChanged", "InsertLeave", "CursorHold", "LspAttach", "BufEnter" },
+							{
+								buffer = bufnr,
+								callback = function()
+									if check_codelens_support() then
+										vim.lsp.codelens.refresh({ bufnr = bufnr })
+									end
+								end,
+							}
+						)
+						vim.api.nvim_exec_autocmds("User", { pattern = "LspAttached" })
+					end)
+				end,
+			},
+		},
+	},
+	{
+		"obsidian-nvim/obsidian.nvim",
+		version = "*",
+		opts = {
+			workspaces = {
+				{
+					name = "SecondBrain",
+					path = "~/second-brain",
+					overrides = {
+						notes_subdir = "notes",
+					},
+				},
+			},
+			daily_notes = {
+				folder = "notes/dailies",
+			},
+			open_notes_in = "vsplit",
+			checkbox = {
+				order = { " ", "x" },
+			},
+		},
+	},
 }
