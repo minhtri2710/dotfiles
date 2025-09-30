9c93f29b228bb2698fadd521f237cb1b06586ecc
Tri.Tran <inpro2710@gmail.com>
2025-09-30 22:45:48 +0700

    chore(nvim): :hammer: update config nvim

--- dot_config/nvim/lua/plugins/sidekick.lua
+++ dot_config/nvim/lua/plugins/sidekick.lua
@@ -4,23 +4,6 @@ return {
     opts = {},
     keys = {
       {
-        "<tab>",
-        function()
-          if require("sidekick").nes_jump_or_apply() then
-            return
-          end
-
-          if vim.lsp.inline_completion.get() then
-            return
-          end
-
-          return "<tab>"
-        end,
-        mode = { "i", "n" },
-        expr = true,
-        desc = "Goto/Apply Next Edit Suggestion",
-      },
-      {
         "<leader>as",
         function()
           require("sidekick.cli").toggle({ focus = true })
--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "fei6409/log-highlight.nvim",
+    opts = {},
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
