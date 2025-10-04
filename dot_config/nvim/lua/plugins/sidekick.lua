ed926d6884263ee4f6ed9b68ffeb2a002c8581aa
Tri.Tran <inpro2710@gmail.com>
2025-10-04 23:27:18 +0700

    chore: :hammer: update config tmux, sidekick, mcp

--- dot_config/nvim/lua/plugins/sidekick.lua
+++ dot_config/nvim/lua/plugins/sidekick.lua
@@ -9,20 +9,88 @@ return {
         },
       },
     },
-    keys = {
-      {
-        "<leader>aa",
-        false,
-      },
-      {
-        "<leader>as",
-        function()
-          require("sidekick.cli").toggle({ focus = true })
-        end,
-        desc = "Sidekick Toggle",
-        mode = { "n" },
-      },
-    },
+    keys = function()
+      return {
+        {
+          "<tab>",
+          function()
+            -- if there is a next edit, jump to it, otherwise apply it if any
+            if require("sidekick").nes_jump_or_apply() then
+              return -- jumped or applied
+            end
+            -- if you are using Neovim's native inline completions
+            if vim.lsp.inline_completion.get() then
+              return
+            end
+            -- fall back to normal tab
+            return "<tab>"
+          end,
+          mode = { "i", "n" },
+          expr = true,
+          desc = "Goto/Apply Next Edit Suggestion",
+        },
+        {
+          "<leader>as",
+          nil,
+          desc = "+Sidekick",
+          mode = { "n", "v" },
+        },
+        {
+          "<leader>asa",
+          function()
+            require("sidekick.cli").toggle({ filter = { installed = true }, focus = true })
+          end,
+          desc = "Sidekick Toggle",
+          mode = { "n" },
+        },
+        {
+          "<leader>ass",
+          function()
+            require("sidekick.cli").select({ filter = { installed = true } })
+          end,
+          desc = "Select CLI",
+        },
+        {
+          "<leader>ast",
+          function()
+            require("sidekick.cli").send({ msg = "{this}" })
+          end,
+          mode = { "x", "n" },
+          desc = "Send This",
+        },
+        {
+          "<leader>asv",
+          function()
+            require("sidekick.cli").send({ msg = "{selection}" })
+          end,
+          mode = { "x" },
+          desc = "Send Visual Selection",
+        },
+        {
+          "<leader>asp",
+          function()
+            require("sidekick.cli").prompt()
+          end,
+          mode = { "n", "x" },
+          desc = "Sidekick Select Prompt",
+        },
+        {
+          "<c-.>",
+          function()
+            require("sidekick.cli").focus()
+          end,
+          mode = { "n", "x", "i", "t" },
+          desc = "Sidekick Switch Focus",
+        },
+        {
+          "<leader>aso",
+          function()
+            require("sidekick.cli").toggle({ name = "opencode", focus = true })
+          end,
+          desc = "Sidekick Opencode Toggle",
+        },
+      }
+    end,
   },
   {
     "saghen/blink.cmp",
